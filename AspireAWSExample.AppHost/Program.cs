9eed5f4fc8882cc618b38a5c74ffaeca9cc9b482
Tobotobo <tobotobo.channel@gmail.com>
2025-04-12 04:03:37 +0900

    Aspire.Hosting.AppHost v9.2.0, Aspire.Hosting.AWS v9.1.8 に更新

--- AspireAWSExample.AppHost/Program.cs
+++ AspireAWSExample.AppHost/Program.cs
@@ -4,6 +4,10 @@ using Aspire.Hosting.AWS.Lambda;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddAWSLambdaServiceEmulator(
+    options: new() { Port = 30000 }
+);
+
 // lambdaHandler -> アセンブリ名::フルクラス名::メソッド名
 
 var countUpLambdaFunction = builder.AddAWSLambdaFunction<Projects.AspireAWSExample_CounterLambdaFunction>(
@@ -15,15 +19,13 @@ var weatherForecastLambdaFunction = builder.AddAWSLambdaFunction<Projects.Aspire
     lambdaHandler: "AspireAWSExample.WeatherForecastLambdaFunction::AspireAWSExample.WeatherForecastLambdaFunction.Function::FunctionHandler");
 
 // https://github.com/aws/integrations-on-dotnet-aspire-for-aws/blob/main/src/Aspire.Hosting.AWS/Lambda/APIGatewayExtensions.cs
+var apiGatewayPort = int.Parse(Environment.GetEnvironmentVariable("ASPIRE_AWS_API_GATEWAY_EMULATOR_PORT") ?? "7777");
 var apiGateway = builder.AddAWSAPIGatewayEmulator(
     name: "APIGatewayEmulator",
-    apiGatewayType: APIGatewayType.HttpV2)
+    apiGatewayType: APIGatewayType.HttpV2,
+    options: new() { Port = apiGatewayPort })
     .WithReference(countUpLambdaFunction, Method.Get, "/countup/{x}")
     .WithReference(weatherForecastLambdaFunction, Method.Get, "/weatherforecast");
-
-var apiGatewayPort = int.Parse(Environment.GetEnvironmentVariable("ASPIRE_AWS_API_GATEWAY_EMULATOR_PORT") ?? "7777");
-var endpointAnnotation = apiGateway.Resource.Annotations.OfType<EndpointAnnotation>().First(x => x.Name == "http");
-endpointAnnotation.Port = apiGatewayPort;
 
 var web = builder.AddProject<Projects.AspireAWSExample_Web>(
     name: "Web")
